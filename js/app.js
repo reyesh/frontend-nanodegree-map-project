61b3e3c7c09b3aefa264548ebc27a7d3969f2c61
Reyes H <reyesh@gmail.com>
2015-11-18 16:06:48 -0800

    REFACTOR: can now added notes to map, removed stroke from circles that show location of notes

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ var ViewModel = {
 
 var db = new Firebase('https://map-notes.firebaseio.com/');
 var map;
+var pos;
 
 $(function (){
 
@@ -50,13 +51,13 @@ function createCir(map, pos){
 
 	var cityCircle = new google.maps.Circle({
       strokeColor: '#333E48',
-      strokeOpacity: 0.8,
-      strokeWeight: 1,
+      strokeOpacity: 0.0,
+      strokeWeight: 0,
       fillColor: '#D71472',
       fillOpacity: 0.25,
       map: map,
       center: pos,
-      radius: 5000
+      radius: 1000
     });
 }
 
@@ -65,30 +66,29 @@ function initMap(){
   var map = new google.maps.Map(document.getElementById('map'), {
                       center: {lat: 87.389, lng: -72.094},
                       scrollwheel: false,
-                      zoom: 9
+                      zoom: 12
                       });
 
   // Try HTML5 geolocation.
   if (navigator.geolocation){
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-    //infoWindow.setPosition(pos);
-    //infoWindow.setContent('Location found.');
-    //map.setCenter(pos);
+      //infoWindow.setPosition(pos);
+      //infoWindow.setContent('Location found.');
+      //map.setCenter(pos);
 
-  map.setCenter(pos);
+    map.setCenter(pos);
 
-
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
-  });
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
   } else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
 
   function handleLocationError(browserHasGeolocation, infoWindow, pos){
@@ -100,3 +100,15 @@ function initMap(){
 
   return map;
 }
+
+function loadData (){
+
+  var note = $("#note").val();
+  var date = Date.now();
+  db.push({ note: note, time: date, pos: { lat: pos.lat, lng: pos.lng } });
+  //console.log(pos);
+  return false;
+
+};
+
+$('#noteform').submit(loadData);
