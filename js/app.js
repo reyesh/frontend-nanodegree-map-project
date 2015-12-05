24c6303691d19a059c3f5e4ade62a81c457d3738
Reyes H <reyesh@gmail.com>
2015-12-04 21:29:02 -0800

    FEAT: saved new note to server, delete note from server, animated markers, function to return note id

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 var pos;
+var canPost = false;
 var map = initMap();
 var markerArr = [];
+var markers = [];
+var infowindow = [];
 
 function note(data) {
 
@@ -13,38 +16,129 @@ function note(data) {
     };
     self.time = data.time;
     self.fake = data.fake;
+    self.id = data._id;
     //self.meal = ko.observable(initialMeal);
 }
 
 function ViewModel() {
   var self = this;    
-  self.folders = ['Post', 'View'];
+  self.folders = ['Post', 'View', 'Delete'];
   self.chosenFolderId = ko.observable('View');
 
   // Behaviours
   self.goToFolder = function(folder) { self.chosenFolderId(folder); };
+  self.clickNoteItem = function(noteList){ console.log(noteList.id); };
 
   self.noteList = ko.observableArray([]);
 
   //noteData.forEach(function(noteItem){
-    //self.noteList.push( new note(noteItem));
-  //  createCir(map,noteItem.pos);
-  //});
   $.getJSON("https://openws.herokuapp.com/map_notes?apiKey=1e63b6cd56a33614f063ed928e35f17a", function(allData) {
       var ajaxNotes = $.map(allData, function(item){ 
         item.pos.lat = Number(item.pos.lat);
         item.pos.lng = Number(item.pos.lng);
-        createMrk(item.pos, item.note);
+        markerArr.push(item);
         return new note(item); 
       });
       self.noteList(ajaxNotes);
-  }); 
+  }).done(function(){drop();})
+  .fail(function( jqxhr, textStatus, error ) {
+    var err = textStatus + ", " + error;
+    console.log( "Request Failed: " + err );
+  });
+
+  // Save a new map note to server
+  self.save = function() {
+    if(canPost==true){
+      var date = Date.now();
+      $.ajax("https://openws.herokuapp.com/map_notes?apiKey=1e63b6cd56a33614f063ed928e35f17a", {
+          data: ko.toJSON({ note: $('#note').val(), time: date, pos: pos }),
+          type: "post", contentType: "application/json",
+          success: function(result) { console.log(ko.toJSON({ name: $('#note').val(), location: pos })); }
+      }); 
+    self.getNotes();
+    } else {
+      console.log("location unknown, can't post");
+    }
+  };
+
+  self.deleteNote = function(){
+
+    $.ajax({
+      url: 'https://openws.herokuapp.com/map_notes/' + $('#noteID').val() + '?apiKey=1e63b6cd56a33614f063ed928e35f17a',
+      type: 'DELETE',
+      success: function(result) {
+          // Do something with the result
+         console.log("Product deleted successfully");
+         console.log(result);
+      }
+    });  
+
+  }
+
+  self.getNotes = function(){
+
+    self.noteList() = ko.observableArray([]);
+    markerArr = [];
+    markers = [];
+    infowindow = [];
+
+    $.getJSON("https://openws.herokuapp.com/map_notes?apiKey=1e63b6cd56a33614f063ed928e35f17a", function(allData) {
+        var ajaxNotes = $.map(allData, function(item){ 
+          item.pos.lat = Number(item.pos.lat);
+          item.pos.lng = Number(item.pos.lng);
+          markerArr.push(item);
+          return new note(item); 
+        });
+        self.noteList(ajaxNotes);
+    }).done(function(){drop();})
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+    });   
+
+  }
+
 }
 
+function drop() {
+
+  clearMarkers();
+  for (var i = 0; i < markerArr.length; i++) {
+    console.log(markerArr[i].pos);
+    addMarkerWithTimeout(markerArr[i].note, markerArr[i].pos, i * 400);
+  } 
+}
+
+function addMarkerWithTimeout(note, position, timeout) {
+  window.setTimeout(function() {
+    markers.push(new google.maps.Marker({
+      position: position,
+      map: map,
+      animation: google.maps.Animation.DROP
+    }));
+
+    infowindow.push(new google.maps.InfoWindow({
+      content: "<h3>"+note+"</h3>"
+    }));
+
+    markers[markers.length-1].addListener('click', (function(marCopy, infowCopy){
+      return function(){
+        infowCopy.open(map, marCopy);
+      };
+    })(markers[markers.length-1], infowindow[infowindow.length-1]));
+  }, timeout);
+}
+
+function clearMarkers() { 
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+}
 
 $(function (){
   ko.applyBindings(new ViewModel());
-
+/*
   $.get("https://openws.herokuapp.com/map_notes?apiKey=1e63b6cd56a33614f063ed928e35f17a")
     .done(function(data) {
       console.log("Products retrieved.");
@@ -53,9 +147,10 @@ $(function (){
       })
       //console.log(data[0].note); // Laptop
     });  
+*/
 /*
   $.ajax({
-    url: 'https://openws.herokuapp.com/map_notes/5654ddc267ae1d03004ff097?apiKey=1e63b6cd56a33614f063ed928e35f17a',
+    url: 'https://openws.herokuapp.com/map_notes/565a46572926f603007857b8?apiKey=1e63b6cd56a33614f063ed928e35f17a',
     type: 'DELETE',
     success: function(result) {
         // Do something with the result
@@ -63,7 +158,7 @@ $(function (){
        console.log(result);
     }
   });  
-*/  
+*/
   /*
   allnotes.forEach(function(item){
     console.log(item);
@@ -153,6 +248,7 @@ function initMap(){
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
+      canPost = true;
 
       //infoWindow.setPosition(pos);
       //infoWindow.setContent('Location found.');
