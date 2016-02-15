555cd65bd332645f7d40fcd0441a0a3b2e94cc62
Reyes H <reyesh@gmail.com>
2016-02-14 18:55:10 -0800

    CHORE: setup src directory, and got index.html ready for build

---
+++ gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+
+        jshint: {
+          files: {
+            src: ['Gruntfile.js','src/js/app.js']
+          }
+        },
+        uglify: {
+      			build: {
+      				src: 'src/js/app.js',
+      				dest: 'src/js/app.min.js'
+      			}
+	     	}, 
+		    cssmin:{
+           dist: {
+              options: {
+                 banner: ''
+              },
+              files: {
+                 'src/css/style.min.css': ['src/css/style.css']
+              }
+          }
+        },
+        processhtml: {
+          options: {
+            data: {
+              message: 'Hello world!'
+            }
+          },
+          dist: {
+            files: {
+              'index.html': ['src/index.html']
+            }
+          }
+        } 
+               
+	});
+
+
+    grunt.registerTask('default', ['jshint','uglify','cssmin','processhtml']);
+};
